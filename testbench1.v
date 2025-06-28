4953045c8f1d97c252a1dc09ebbf5bdabb5218b6
anikaverma24 <anikaverma2019@gmail.com>
2025-06-29 01:05:44 +0530

    Update testbench1.v

--- testbench1.v
+++ testbench1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.06.2025 12:18:58
-// Design Name: 
-// Module Name: testbench_MIPS32
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench1;
 
